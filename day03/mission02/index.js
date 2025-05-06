d4de8a656c83c714508d92d85ccbf1d4e9d793a2
dyw0409 <dy_woo0409@naver.com>
2025-05-06 14:24:03 +0900

    js2수정

--- day03/mission02/index.js
+++ day03/mission02/index.js
@@ -4,16 +4,18 @@ const btn = document.querySelector("#box");
 
 btn.addEventListener('mousedown', (e) => {
     e.preventDefault();
+ });
 
-    if (e.button === 0) {
-      box2.textContent = '왼쪽 버튼';
-    } else if (e.button === 1) {
-      box2.textContent = '가운데 버튼';
-    } else if (e.button === 2) {
-      box2.textContent = '오른쪽 버튼';
+ btn.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+
+    if(e.button ===0){
+      btn.textContent="왼쪽버튼";
+    }else if (e.button ===2){
+      btn.textContent="오쪽버튼";
     }
-  });
-btn.addEventListener('mouseup', () => {
-    box2.textContent = '클릭해해해보세요.';
-  });
+ });
 
+ btn.addEventListener('mouseup', ()=> {
+  btn.textContent="클랙해보세요.";
+ });
